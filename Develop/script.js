7ad181821e280cc6a2bd71c3c830f0ebb97c2689
A-MOHAMED14 <amin800@hotmail.co.uk>
2024-05-23 14:26:59 +0100

    Add first prompt for password criteria

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,13 @@ var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
 function writePassword() {
+  var passwordLength = prompt(
+    "Please enter the desired password length. It must be between 8 and 128 characters inclusive."
+  );
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
